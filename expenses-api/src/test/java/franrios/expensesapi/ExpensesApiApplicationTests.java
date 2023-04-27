55e61e27bb798872e1ebf5156c943e4892ea827b
Francisco Rios <fcrios145@hotmail.com>
2023-04-27 12:59:55 -0700

    git commit init

---
+++ expenses-api/src/test/java/franrios/expensesapi/ExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package franrios.expensesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
